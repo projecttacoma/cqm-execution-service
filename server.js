8c1cd8e558537a9cd12699ab57d682ced830003d
holmesie <aholmes@mitre.org>
2018-06-28 09:35:01 -0400

    Fixed issue with request body for POST

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-const express = require('express');
 //const models = require('cqm-models'); TODO: Add this when cqm-models is in NPM
 //const engine = require('js-ecqm-engine'); TODO: Add this when js-ecqm-engine is in NPM
-const app = express();
+var app = require('express')();
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
 
-app.use(express.json());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
 
 LISTEN_PORT = process.env.CQM_EXECUTION_SERVICE_PORT || 8081; // Port to listen on
 REQUIRED_PARAMS = ['measure', 'valueSets', 'patients']; // Required params for calculation
@@ -32,36 +35,36 @@ app.get('/version', function (request, response) {
  * @bodyparam patients - an array of cqm-models based patients to calculate for.
  * @bodyparam options - optional params for things like generating pretty results.
  */
-app.post('/calculate', function (request, response) {
-    // Certain params are required for this action, make sure they exist.
-    let missing = []
-    REQUIRED_PARAMS.forEach(function (param) {
-      if (!request.body.hasOwnProperty(param)) {
-        missing.push(param);
-      }
-    });
-    // If there are missing params, return a 400 with a description of which
-    // params were missing.
-    if (missing.length) {
-      response.status(400).send({
-        'error': `Missing required parameter(s): ${missing.join(', ')}`, 'request': request.body
-      });
-      return;
+app.post('/calculate', upload.array(), function (request, response) {
+  // Certain params are required for this action, make sure they exist.
+  let missing = []
+  REQUIRED_PARAMS.forEach(function (param) {
+    if (!request.body[param]) {
+      missing.push(param);
     }
-
-    // Grab params from request.
-    var measure = request.body['measure'];
-    var valueSets = request.body['valueSets'];
-    var patients = request.body['patients'];
-    var options = request.body['options'] || {};
-
-    // TODO: Incorporate js-ecqm-engine to actually do calculation.
-    response.send({
-      'measure': JSON.stringify(measure),
-      'valueSets': JSON.stringify(valueSets),
-      'patients': JSON.stringify(patients),
-      'options': JSON.stringify(options)
+  });
+  // If there are missing params, return a 400 with a description of which
+  // params were missing.
+  if (missing.length) {
+    response.status(400).send({
+      'error': `Missing required parameter(s): ${missing.join(', ')}`, 'request': request.body
     });
+    return;
+  }
+
+  // Grab params from request.
+  var measure = request.body['measure'];
+  var valueSets = request.body['valueSets'];
+  var patients = request.body['patients'];
+  var options = request.body['options'] || {};
+
+  // TODO: Incorporate js-ecqm-engine to actually do calculation.
+  response.json({
+    'measure': measure,
+    'valueSets': valueSets,
+    'patients': patients,
+    'options': options
+  });
 });
 
 app.listen(LISTEN_PORT, () => console.log('cqm-execution-service is now listening on port ' + LISTEN_PORT));
