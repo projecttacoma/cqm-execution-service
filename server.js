cc95669057ffb1bcd3ef5a36f77bae9146f088a4
David Czulada <dczulada@mitre.org>
2021-07-26 10:29:10 -0400

    Support QDM 5.6 with cqm-execution v4

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(compression());
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb', parameterLimit: '5000' }));
 
-const LISTEN_PORT = process.env.CQM_EXECUTION_SERVICE_PORT || 8081; // Port to listen on
+const LISTEN_PORT = process.env.CQM_EXECUTION_SERVICE_PORT || 8082; // Port to listen on
 const REQUIRED_PARAMS = ['measure', 'valueSets', 'patients']; // Required params for calculation
 
 
